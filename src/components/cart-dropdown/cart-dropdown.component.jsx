c77ecfd488f1d6f20a03d3038ffe092530bdfd72
SonnyFishback <sonny.fishback7@gmail.com>
2021-01-17 04:56:39 -0500

    creating dropdown list for shoping cart icon

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import  { CustomButtom } from "../custom-button/custom-button.component";
+import "./cart-dropdown.syles.scss";
+
+
+export const CartDropdown = () => {
+    return(
+        <div className="cart-dropdown">
+            <div className="cart-items"/>
+            <CustomButtom>Go TO CHECKOUT</CustomButtom>
+        </div>
+    )
+}
